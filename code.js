bcd43635725bd8db98a516f39c4d9cafd22d9ba2
Thierry Dang <thierry.dang@external.engie.com>
2024-10-17 17:46:06 +0200

    calculatrice

--- code.js
+++ code.js
@@ -1,2 +1,30 @@
-console.log("code.js ok");
-alert("code js est inclu");
+window.onload = function () {
+    // do the work after everything was loaded (DOM, media elements)    
+    document.querySelector('button').onclick = function() {
+
+
+        var a =  parseInt(document.getElementById('a').value);
+        var b = parseInt(document.getElementById('b').value);
+        
+        if (isNaN(a) || isNaN(b)) {
+            document.getElementById('result').innerHTML = 'Invalid input';
+            return;
+        }
+        switch (document.getElementById('operations').value) {
+            case 'plus':
+                document.getElementById('result').innerHTML = a + b;
+                break;
+            case 'moins':
+                document.getElementById('result').innerHTML = a - b;
+                break;
+            case 'multiplication':
+                document.getElementById('result').innerHTML = a * b;
+                break;
+            case 'division':
+                document.getElementById('result').innerHTML = a / b;
+                break;
+            default:
+                document.getElementById('result').innerHTML = 'Invalid operation';
+        }
+    };
+};
